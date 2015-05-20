908c2c892ac7833e7e35e9ee2869f2c0d8857e18
Nathan Bone <nathanbone88@gmail.com>
2015-05-19 20:37:45 -0400

    added working score

---
+++ Assets/ScoreText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreText : MonoBehaviour {
+
+	public PlayerNumbers playerNumber;
+	private Text text; 
+	public int score;
+
+	void OnEnable() {
+		Messenger.AddListener<PlayerNumbers>("Goal", onGoal);
+		text = GetComponent<Text>();
+		updateScoreText();
+
+	}
+
+	void OnDisable(){
+		Messenger.RemoveListener<PlayerNumbers>("Goal", onGoal);
+	}
+
+	void onGoal(PlayerNumbers playerNum){
+		if( this.playerNumber == playerNum) {
+			score++;
+			updateScoreText();
+		}
+	}
+	void updateScoreText(){
+		text.text = score.ToString();
+	}
+
+
+
+}
